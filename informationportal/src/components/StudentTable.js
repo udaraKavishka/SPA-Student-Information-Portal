1c712bcf6f1974c724864453ae23d104e1a9577c
Udara Nalawansa <udarakavishka13@gmail.com>
2024-11-19 09:10:50 +0000

    fix: fix rendering of student data in StudentTable component

--- informationportal/src/components/StudentTable.js
+++ informationportal/src/components/StudentTable.js
@@ -3,7 +3,7 @@ export default function StudentTable(props) {
     
     return(
         <div className="outerDiv">
-            <div className="leftDiv" > </div>
+            <div className="leftDiv" > 
                 <table border="1">
                 <thead>
                     <tr>
@@ -16,20 +16,20 @@ export default function StudentTable(props) {
                     </thead>
                     <tbody>
                         {
-                            props.students.map((student) => {
-                                return(
+                            props.students.map(student =>
+                                
                                     <tr>
                                         <td>{student.firstName}</td>
                                         <td>{student.lastName}</td>
                                         <td>{student.course}</td>
                                         <td>{student.address.country}</td>
-                                        <td><img src={student.profilePic} alt="profile" width="50px" height="50px"/></td>
                                     </tr>
-                                );
-                            })
+                            ) 
+                            
                         }
                     </tbody>
                 </table>
+                </div>
         </div>
     );
 }
