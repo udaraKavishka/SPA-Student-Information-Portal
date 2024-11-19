5a7065867590c796a3cc1f88c12a49300891e2e7
Udara Nalawansa <udarakavishka13@gmail.com>
2024-11-19 08:48:31 +0000

    feat: enhance StudentTable component layout and styling

--- informationportal/src/components/StudentTable.js
+++ informationportal/src/components/StudentTable.js
@@ -1,8 +1,18 @@
-export default function StudentTable() {
+import '../assets/css/compo.css';
+export default function StudentTable(props) {
     
     return(
-        <div>
-            
+        <div className="outerDiv">
+            <div className="leftDiv" > </div>
+                <table>
+                    <tr>
+                        <td>First Name</td>
+                        <td>Last Name</td>
+                        <td>Course</td>
+                        <td>Country</td>
+                        <td>Profile</td>
+                    </tr>
+                </table>
         </div>
     );
 }
