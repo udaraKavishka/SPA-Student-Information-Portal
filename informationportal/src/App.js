a739e0f97678a3d4a145f8459af4bca15088773c
Udara Nalawansa <udarakavishka13@gmail.com>
2024-11-19 18:44:41 +0530

    feat: add font size adjustment buttons to App component

--- informationportal/src/App.js
+++ informationportal/src/App.js
@@ -1,11 +1,20 @@
+import { useState } from 'react';
 import './App.css';
 import StudentTable from './components/StudentTable';
 import {students} from './data/StudentsDb'
 
 function App() {
+  const [fontSize, setFontSize] = useState(12);
   return (
     <div className="App">
       <h1>Student Information Portal</h1>
+      <hr/>
+      <div align="left">
+        Font Size:
+        <button>S</button>
+        <button>M</button>
+        <button>L</button>
+      </div>
       <hr/>
       <table width="100%">
         <tbody>
